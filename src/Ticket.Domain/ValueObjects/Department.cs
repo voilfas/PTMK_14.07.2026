87c5ff01fd0d6a706d85dae0c9a313bf87a7f8a0
Artyom <sobkalovt@bk.ru>
2026-07-16 14:44:00 +0300

    feat(domain): add department value object

---
+++ src/Ticket.Domain/ValueObjects/Department.cs
@@ -0,0 +1,36 @@
+﻿using Ticket.Domain.Common;
+using Ticket.Domain.Errors;
+
+namespace Ticket.Domain.ValueObjects;
+
+public record Department
+{
+    //private bool IsActive { get; } можно сделать сущностью
+    private string Name { get; }
+    private string Code { get;  }
+
+    private Department(string name, string code)
+    {
+        Name = name;
+        Code = code;
+    }
+
+    public static Result<Department> Create(string name, string code)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            return Result<Department>.Failure(ErrorsDepartment.EmptyName);
+
+        if (string.IsNullOrWhiteSpace(code))
+            return Result<Department>.Failure(ErrorsDepartment.EmptyCode);
+
+        if (name.Length is < 2 or > 30)
+            return Result<Department>.Failure(ErrorsDepartment.IncorrectName);
+
+        if (code.Length is < 5 or > 15)
+            return Result<Department>.Failure(ErrorsDepartment.IncorrectCode);
+
+        return Result<Department>.Success(new Department(name, code));
+    }
+
+    public override string ToString() => $"{Name} {Code}";
+}
