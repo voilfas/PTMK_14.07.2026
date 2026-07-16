9d005eea60749e5c98125fc1f91492d7c099dcba
Artyom <sobkalovt@bk.ru>
2026-07-16 14:48:04 +0300

    feat(domain): add errors types for ticket entity

---
+++ src/Ticket.Domain/Errors/ErrorsTicket.cs
@@ -0,0 +1,14 @@
+﻿using Ticket.Domain.Common;
+
+namespace Ticket.Domain.Errors;
+
+public static class ErrorsTicket
+{
+    public static readonly Error EmptyAuthorId = new Error("Ticket.EmptyAuthorId", "Id of author can't be empty");
+    public static readonly Error EmptyExecutorId = new Error("Ticket.EmptyExecutorId", "Id of executor can't be empty");
+    
+    public static readonly Error EmptyDescription = new Error("Ticket.EmptyDescription", "Description can't be empty");
+    
+    public static readonly Error EmptyDeadLine = new Error("Ticket.EmptyDeadLine", "DeadLine can't be empty");
+    public static readonly Error IncorrectDeadLine = new Error("Ticket.IncorrectDeadLine", "DeadLine can't be earlier than the current day and later than 1 year");
+}
