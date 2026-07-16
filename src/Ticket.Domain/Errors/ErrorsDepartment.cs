47fb7ea7837314abc948eb76214848489b93025d
Artyom <sobkalovt@bk.ru>
2026-07-16 14:45:39 +0300

    feat(domain): add errors types for department and employee

---
+++ src/Ticket.Domain/Errors/ErrorsDepartment.cs
@@ -0,0 +1,14 @@
+﻿using Ticket.Domain.Common;
+
+namespace Ticket.Domain.Errors;
+
+public static class ErrorsDepartment
+{
+    public static readonly Error EmptyName = new Error("Department.EmptyName", "Name of department can't be empty");
+    public static readonly Error EmptyCode = new Error("Department.EmptyCode", "Code of department can't be empty");
+    
+    public static readonly Error IncorrectName = new Error("Department.IncorrectName", "Name should contain 2 - 30 symbols");
+    public static readonly Error IncorrectCode = new Error("Department.IncorrectCode", "First name should contain 5 - 15 symbols");
+    
+    public static readonly Error Inactive = new Error("Department.Inactive", "Can't create inactive department");
+}
