0ef0669b92dae8b0133e36d8b98da0061eec8f62
Artyom <sobkalovt@bk.ru>
2026-07-16 14:42:23 +0300

    feat(domain): implement resul pattern

---
+++ src/Ticket.Domain/Common/Error.cs
@@ -0,0 +1,13 @@
+﻿namespace Ticket.Domain.Common;
+
+public class Error
+{
+    public string Message { get; private set; }
+    public string Code { get; private set; }
+
+    public Error(string code, string message)
+    {
+        Message = message;
+        Code = code;
+    }
+}
---
+++ src/Ticket.Domain/Common/ResultT.cs
@@ -0,0 +1,20 @@
+﻿namespace Ticket.Domain.Common;
+
+public class Result<T> : Result
+{
+    public T? Value { get; }
+
+    private Result(T value) : base(isSuccess: true, error: null)
+    {
+        Value = value;
+    }
+
+    private Result(Error error): base(isSuccess: false, error)
+    {
+        Value = default;
+    }
+    
+    public static Result<T> Success(T value) => new Result<T>(value);
+    
+    public static Result<T> Failure(Error error) => new Result<T>(error);
+}
