259675db247fb1af12bd83d874e849ed1efd8e31
Artyom <sobkalovt@bk.ru>
2026-07-16 21:24:24 +0300

    feat(domain): add methods for domain entity - ticket

--- src/Ticket.Domain/Entities/Ticket.cs
+++ src/Ticket.Domain/Entities/Ticket.cs
@@ -10,13 +10,13 @@ public class Ticket
     public Guid Id { get; private set; }
     public TicketNumber TicketNumber { get; private set; }
     public DateTime CreatedAt { get; private set; }
-    public Employee AuthorId { get; private set; }
-    public Employee ExecutorId { get; private set; }
+    public Guid AuthorId { get; private set; }
+    public Guid? ExecutorId { get; private set; }
     public string Description { get; private set; }
     public DateTime DeadLine { get; private set; }
     public TicketStatus Status { get; private set; }
 
-    private Ticket(Employee authorId, Employee executorId, string description, DateTime deadLine)
+    private Ticket(Guid authorId, string description, DateTime deadLine, Guid? executorId = null)
     {
         Id = Guid.NewGuid();
         TicketNumber = TicketNumber.Generate(Id);
@@ -28,20 +28,59 @@ public class Ticket
         Status = TicketStatus.New;
     }
 
-    public static Result<Ticket> Create(Employee? authorId, Employee? executorId, string? description, DateTime deadLine)
+    public static Result<Ticket> Create(Guid authorId, Guid? executorId, string description, DateTime deadLine)
     {
-        if (authorId is null)
+        if (authorId == Guid.Empty)
             return Result<Ticket>.Failure(ErrorsTicket.EmptyAuthorId);
-        
-        if (executorId is null)
+
+        if (executorId.HasValue && executorId.Value == Guid.Empty)
             return Result<Ticket>.Failure(ErrorsTicket.EmptyExecutorId);
         
         if (string.IsNullOrWhiteSpace(description))
             return Result<Ticket>.Failure(ErrorsTicket.EmptyDescription);
+        
+        if (description.Length is < 3 or > 300)
+            return Result<Ticket>.Failure(ErrorsTicket.IncorrectDescription);
 
         if (deadLine <= DateTime.UtcNow || deadLine > DateTime.UtcNow.AddYears(1))
             return Result<Ticket>.Failure(ErrorsTicket.IncorrectDeadLine);
 
-        return Result<Ticket>.Success(new Ticket(authorId, executorId, description, deadLine));
+        return Result<Ticket>.Success(new Ticket(authorId, description, deadLine, executorId));
+    }
+    
+    public Result ChangeStatus(TicketStatus status)
+    {
+        if (Status == status)
+            return Result.Success();
+        
+        if (Status == TicketStatus.New && status == TicketStatus.Completed)
+            return Result.Failure(ErrorsTicket.FailStatusFromNewToCompleted);
+
+        if (Status == TicketStatus.Completed && status == TicketStatus.InProgress)
+            return Result.Failure(ErrorsTicket.FailStatusFromCompletedToInProgress);
+
+        if (!((Status == TicketStatus.InProgress && status == TicketStatus.Completed) ||
+              (Status == TicketStatus.New && status == TicketStatus.InProgress)))
+            return Result.Failure(ErrorsTicket.InvalidStatusTransition);
+        
+        Status = status;
+        
+        return Result.Success();
+    }
+
+    public Result ChangeExecutor(Guid executorId)
+    {
+        if (executorId == Guid.Empty)
+            return Result.Failure(ErrorsTicket.EmptyExecutorId);
+        
+        if (Status == TicketStatus.Completed)
+            return Result.Failure(ErrorsTicket.InvalidChangeExecutor);
+
+        if (executorId == ExecutorId)
+            return Result.Success();
+        
+        ExecutorId = executorId;
+        
+        return Result.Success();
     }
 }
--- src/Ticket.Domain/Errors/ErrorsTicket.cs
+++ src/Ticket.Domain/Errors/ErrorsTicket.cs
@@ -5,10 +5,17 @@ namespace Ticket.Domain.Errors;
 public static class ErrorsTicket
 {
     public static readonly Error EmptyAuthorId = new Error("Ticket.EmptyAuthorId", "Id of author can't be empty");
-    public static readonly Error EmptyExecutorId = new Error("Ticket.EmptyExecutorId", "Id of executor can't be empty");
+    public static readonly Error EmptyExecutorId = new Error("Ticket.EmptyExecutorId", "Id of executor is present but it's empty");
     
     public static readonly Error EmptyDescription = new Error("Ticket.EmptyDescription", "Description can't be empty");
+    public static readonly Error IncorrectDescription = new Error("Ticket.IncorrectDescription", "Description should contain from 3 - 300 symbols");
     
     public static readonly Error EmptyDeadLine = new Error("Ticket.EmptyDeadLine", "DeadLine can't be empty");
     public static readonly Error IncorrectDeadLine = new Error("Ticket.IncorrectDeadLine", "DeadLine can't be earlier than the current day and later than 1 year");
+    
+    public static readonly Error FailStatusFromNewToCompleted = new Error("Ticket.FailStatusFromNewToCompleted", "Can't change status from new to completed");
+    public static readonly Error FailStatusFromCompletedToInProgress = new Error("Ticket.FailStatusFromCompletedToInProgress", "Can't change status from completed to in progress");
+    public static readonly Error InvalidStatusTransition = new Error("Ticket.InvalidStatusTransition", "This transition is invalid");
+    
+    public static readonly Error InvalidChangeExecutor = new Error("Ticket.InvalidChangeExecutor", "Can't change the executor when ticket is completed");
 }
