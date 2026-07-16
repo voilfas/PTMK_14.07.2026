64d72ad3b1ca4883c3335c89f1bb917205695ef2
Artyom <sobkalovt@bk.ru>
2026-07-16 14:44:17 +0300

    feat(domain): add fullname value object

---
+++ src/Ticket.Domain/ValueObjects/FullName.cs
@@ -0,0 +1,43 @@
+﻿using Ticket.Domain.Common;
+using Ticket.Domain.Errors;
+
+namespace Ticket.Domain.ValueObjects;
+
+public record FullName
+{
+    private string FirstName { get; }
+    private string LastName { get; }
+    private string Surname { get; }
+
+    private FullName(string firstName, string lastName, string surname)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Surname = surname;
+    }
+
+    public static Result<FullName> Create(string firstName, string lastName, string surname)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            return Result<FullName>.Failure(ErrorsEmployee.EmptyFirstName);
+
+        if (string.IsNullOrWhiteSpace(lastName))
+            return Result<FullName>.Failure(ErrorsEmployee.EmptyLastName);
+        
+        if (string.IsNullOrWhiteSpace(surname))
+            return Result<FullName>.Failure(ErrorsEmployee.EmptySurname);
+
+        if (firstName.Length is < 2 or > 20)
+            return Result<FullName>.Failure(ErrorsEmployee.IncorrectFirstName);
+        
+        if (lastName.Length is < 2 or > 20)
+            return Result<FullName>.Failure(ErrorsEmployee.IncorrectLasName);
+
+        if (surname.Length is < 2 or > 20)
+            return Result<FullName>.Failure(ErrorsEmployee.IncorrectSurname);
+
+        return Result<FullName>.Success(new FullName(firstName, lastName, surname));
+    }
+    
+    public override string ToString() => $"{FirstName} {LastName} {Surname}";
+}
