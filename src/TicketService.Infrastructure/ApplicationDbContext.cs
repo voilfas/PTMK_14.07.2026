60415c5cb13da2d8b3484f207762e40d127ad21a
Artyom <sobkalovt@bk.ru>
2026-07-23 13:50:19 +0300

    refactor(infrast): add TicketExecutors

--- src/TicketService.Infrastructure/ApplicationDbContext.cs
+++ src/TicketService.Infrastructure/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TicketService.Domain.Entities;
 
-namespace TicketService.Infrastructure.Persistence;
+namespace TicketService.Infrastructure;
 
 public class ApplicationDbContext : DbContext
 {
@@ -9,6 +9,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<Department> Departments => Set<Department>();
     public DbSet<Employee> Employees => Set<Employee>();
     public DbSet<Position> Positions => Set<Position>();
+    public DbSet<TicketExecutor> TicketExecutors => Set<TicketExecutor>();
     
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
