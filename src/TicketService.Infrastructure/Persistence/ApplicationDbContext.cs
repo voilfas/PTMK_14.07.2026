6e5b29c6092ebe524e4d26194a8d11ced48fcae8
Artyom <sobkalovt@bk.ru>
2026-07-23 11:41:11 +0300

    feat(infrast): add base structure and ApplicationDbContext

---
+++ src/TicketService.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketService.Domain.Entities;
+
+namespace TicketService.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Ticket>  Tickets => Set<Ticket>();
+    public DbSet<Department> Departments => Set<Department>();
+    public DbSet<Employee> Employees => Set<Employee>();
+    public DbSet<Position> Positions => Set<Position>();
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
