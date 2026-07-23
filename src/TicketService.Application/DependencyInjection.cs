a4785afc781ade37a954eff8f4e3a5a570b3dc64
Artyom <sobkalovt@bk.ru>
2026-07-23 19:41:08 +0300

    feat(application): add dependency for Mediatr

---
+++ src/TicketService.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TicketService.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+        
+        return services;
+    }
+}
