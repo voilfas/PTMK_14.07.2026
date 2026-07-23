1e20b666d543e4642d5012bc8c643c7857a1259e
Artyom <sobkalovt@bk.ru>
2026-07-23 16:49:01 +0300

    feat(infrast): add DI

---
+++ src/TicketService.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TicketService.Application.Abstractions;
+using TicketService.Application.Abstractions.Persistence.Commands;
+using TicketService.Application.Abstractions.Persistence.Queries;
+using TicketService.Infrastructure.Persistence;
+using TicketService.Infrastructure.Persistence.Repositories;
+
+namespace TicketService.Infrastructure.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database")
+                               ?? throw new InvalidOperationException(
+                                   "Connection string 'Database' was not found.");
+        
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+        
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        
+        services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+        services.AddScoped<IDepartmentReadRepository, DepartmentReadRepository>();
+        
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        services.AddScoped<IEmployeeReadRepository, EmployeeReadRepository>();
+        
+        services.AddScoped<IPositionRepository, PositionRepository>();
+        services.AddScoped<IPositionReadRepository, PositionReadRepository>();
+        
+        services.AddScoped<ITicketRepository, TicketRepository>();
+        services.AddScoped<ITicketReadRepository, TicketReadRepository>();
+        
+        return services;
+    }
+}
