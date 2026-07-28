53e048de88f43103f5f42bff2f8ef7b2f8a8d72b
Artyom <sobkalovt@bk.ru>
2026-07-28 13:57:33 +0300

    feat(application): implement cache in departments handler

--- src/TicketService.Application/UseCases/Departments/Queries/GetDepartments/GetDepartmentsHandler.cs
+++ src/TicketService.Application/UseCases/Departments/Queries/GetDepartments/GetDepartmentsHandler.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using TicketService.Application.Abstractions.Persistence.Queries;
 using TicketService.Application.Common;
+using TicketService.Application.Common.Cache;
 using TicketService.Application.ResponceDTOs;
 
 namespace TicketService.Application.UseCases.Departments.Queries.GetDepartments;
@@ -9,30 +10,31 @@ namespace TicketService.Application.UseCases.Departments.Queries.GetDepartments;
 public class GetDepartmentsHandler : IRequestHandler<GetDepartmentsQuery, PageResult<DepartmentDto>>
 {
     private readonly IDepartmentReadRepository _repository;
-    private readonly ILogger<GetDepartmentsHandler> _logger;
+    private readonly ICacheService _cache;
 
     public GetDepartmentsHandler(
         IDepartmentReadRepository repository,
-        ILogger<GetDepartmentsHandler> logger)
+        ICacheService cache)
     {
         _repository = repository;
-        _logger = logger;
+        _cache = cache;
     }
 
     public async Task<PageResult<DepartmentDto>> Handle(
         GetDepartmentsQuery query,
         CancellationToken cancellationToken)
     {
-        /*_logger.LogInformation(
-            "Getting departments. Page={Page}, PageSize={PageSize}",
-            query.Page,
-            query.PageSize);*/
+
+        var cacheKey =  $"departments:{query.Page}:{query.PageSize}";
+        
+        var cachedDepartments  = await _cache.GetAsync<PageResult<DepartmentDto>>(cacheKey);
+
+        if (cachedDepartments  is not null)
+            return cachedDepartments ;
         
         var departments =  await _repository.GetAllAsync(query.Page, query.PageSize, cancellationToken);
         
-        /*_logger.LogInformation(
-            "Retrieved {Count} departments",
-            departments.TotalCount);*/
+        await _cache.SetAsync(cacheKey,  departments, TimeSpan.FromMinutes(5));
         
         return departments;
     }
